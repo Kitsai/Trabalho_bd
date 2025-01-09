d53b0e2e0d837fbd5f2e4a90698b9d25e7483cdc
Kitsai <luck3801@gmail.com>
2025-01-09 13:36:48 -0300

    changes and base backend

---
+++ Back/src/app.ts
@@ -0,0 +1,14 @@
+import express from "express"
+import cors from "cors"
+import { routers } from "./routers";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routers);
+const port = 3000;
+
+app.listen(port, () => {
+  console.log("Aplicação rodando na porta: " + port);
+});
