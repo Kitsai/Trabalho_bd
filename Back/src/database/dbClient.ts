c15b0710a1c1e4b2d6ddff6f42ad9933b3b1b693
Kitsai <luck3801@gmail.com>
2025-01-09 13:07:07 -0300

    Back initial commit

---
+++ Back/src/database/dbClient.ts
@@ -0,0 +1,24 @@
+import { Pool } from "pg"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+export class DbClient {
+  private static _instance: DbClient;
+
+  private _pool: Pool;
+
+  private constructor() {
+    this._pool = new Pool({
+      host: process.env.DATABASE_HOST,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT) : undefined
+    })
+  }
+
+  public static get Instance(): DbClient {
+    return DbClient._instance || new DbClient();
+  }
+}
