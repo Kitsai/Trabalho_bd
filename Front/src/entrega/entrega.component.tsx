90612accd03a0238da27741ebbbc539491234423
Kitsai <lucas.rocha.3801@gmail.com>
2025-02-08 18:11:51 -0300

    asasasa

--- Front/src/entrega/entrega.component.tsx
+++ Front/src/entrega/entrega.component.tsx
@@ -12,22 +12,20 @@ interface EntregaListItemProps extends ComponentProps<'li'> {
 export function EntregaListItem({ entrega, openUpdate, handleDelete, ...props }: EntregaListItemProps) {
   return (
     <li key={entrega.codpedent} {...props} className="outline-zinc-950 bg-amber-50 my-2 p-5 flex items-center justify-start gap-x-12 rounded-2xl">
-      <div>
-        <div className="space-x-3">
-          <span><strong>{entrega.codpedent} - {entrega.codcli} - {entrega.time.toString()}</strong></span>
-          <button onClick={() => openUpdate({ codpedent: entrega.codpedent, codcli: entrega.codcli, time: entrega.time })}>Atualizar</button>
-          <button onClick={() => handleDelete(entrega.codpedent)}>Deletar</button>
-        </div>
-        <div className="flex ml-12 mr-12 flex-col gap-y-2" >
-          {entrega.alimentos.map((ali: PedidoAlimento) => {
-            return (
-              <div>
-                <span>{ali.codali}</span> - <span>{ali.qtd}</span>
-              </div>
-            )
-          }
-          )}
-        </div>
+      <div className="space-x-3">
+        <span><strong>{entrega.codpedent} - {entrega.codcli} - {entrega.time.toString()}</strong></span>
+        <button onClick={() => openUpdate({ codpedent: entrega.codpedent, codcli: entrega.codcli, time: entrega.time })}>Atualizar</button>
+        <button onClick={() => handleDelete(entrega.codpedent)}>Deletar</button>
+      </div>
+      <div className="flex ml-12 mr-12 flex-col gap-y-2" >
+        {entrega.alimentos.map((ali: PedidoAlimento) => {
+          return (
+            <div>
+              <span>{ali.codali}</span> - <span>{ali.qtd}</span>
+            </div>
+          )
+        }
+        )}
       </div>
     </li>
   )
