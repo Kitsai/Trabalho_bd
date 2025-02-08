8f977d701894cfd6c6037a4b4346faf14744f130
gabie762 <gabihenriques821@gmail.com>
2025-02-08 18:30:55 -0300

    Refactor ClienteListItem layout for improved styling and button alignment

--- Front/src/components/cliente/cliente.component.tsx
+++ Front/src/components/cliente/cliente.component.tsx
@@ -9,15 +9,20 @@ interface ClienteListItemProps extends ComponentProps<'li'> {
 
 export function ClienteListItem({ cliente, openUpdate, handleDelete, ...props }: ClienteListItemProps) {
   return (
-    <li key={cliente.codcli} {...props} className="outline-zinc-950 bg-amber-50 my-2 p-5 rounded-2xl">
-      <span className="rounded-l text-black outline-zinc-900 size-64">{cliente.nome}</span>
-      <br />
-      <span>  {cliente.endereco}</span>
-      <span>{cliente.codent}</span>
-      <div className="flex justify-around items-baseline rounded-2xl">
-        <button className='hover:cursor-grab outline-black outline-1' onClick={() => { openUpdate(cliente) }}>Editar</button>
-        <button className='hover:cursor-grab outline-black outline-1' onClick={() => { handleDelete(cliente.codcli) }}>Deletar</button>
-      </div>
+    <div className="">
+      <li key={cliente.codcli} {...props} className="outline-zinc-950 bg-amber-50 my-2 p-5 rounded-2xl flex justify-between items-center">
+        <div className="items-center">
+          <span className="rounded-l text-black outline-zinc-900 size-64">{cliente.nome}</span>
+          <br />
+          <span>{cliente.endereco}</span>
+          <span>{cliente.codent}</span>
+        </div>  
+        <div className="flex justify-between space-x-4 items-baseline rounded-2xl">
+          <button className='hover:cursor-grab outline-dark-blue text-dark-blue outline-1 p-1' onClick={() => { openUpdate(cliente) }}>Editar</button>
+          <button className='hover:cursor-grab outline-dark-blue text-dark-blue outline-1 p-1' onClick={() => { handleDelete(cliente.codcli) }}>Deletar</button>
+        </div>
     </li >
+    </div>
+    
   )
 }
