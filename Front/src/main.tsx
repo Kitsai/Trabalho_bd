d6ae87f49fb2002b421942c390edec23ffb1af8c
Kitsai <lucas.rocha.3801@gmail.com>
2025-02-10 10:35:14 -0300

    redirect

--- Front/src/main.tsx
+++ Front/src/main.tsx
@@ -1,19 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route } from 'react-router'
-import { Routes } from 'react-router'
+import { Routes, Navigate } from 'react-router'
 import { ClientePage } from './pages/cliente/cliente.page.tsx'
-import { TesteAlimento } from './tests/alimento.test.tsx'
 import { EntregaPage } from './pages/entrega/entrega.page.tsx'
 import { EntregadorPage } from './pages/entregador/entregador.page.tsx'
-import { TesteMenu } from './tests/menu.teste.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<TesteMenu />} />
+      <Route path='/' element={<Navigate to={'/entrega'} />} />
+
       <Route path='/cliente' element={<ClientePage />} />
-      <Route path='/alimentos' element={<TesteAlimento />} />
       <Route path='/entrega' element={<EntregaPage />}></Route>
       <Route path='/entregador' element={<EntregadorPage />}></Route>
     </Routes>
